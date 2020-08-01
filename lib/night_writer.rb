279c14007bf6d54149739e8280a288103f4b29fe
John Kim <abcdefghijohnk@gmail.com>
2020-08-01 12:49:16 -0600

    edit exist test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,16 @@
 require './lib/file_reader'
 class NightWriter
-  attr_reader :reader
+  attr_reader :file_reader
 
   def initialize
     @reader = FileReader.new
   end
 end
+
+# throw in cmd  in terminal
+# regardless of what it is
+# message.txt or braille.txt
+# will print out the "created ....."
+#
+# night writer - in charge of pasting initial input
+# reader - in charge of reading files
