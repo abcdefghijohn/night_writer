15715417b5bbcebee1e9c3d008c364f4ed57a455
John Kim <abcdefghijohnk@gmail.com>
2020-08-01 12:55:05 -0600

    create file_reader test

--- test/file_reader_test.rb
+++ test/file_reader_test.rb
@@ -1,1 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/file_reader'
 
+class FileReaderTest < MiniTest::Test
+
+  def test_it_exits
+    file_reader = FileReader.new
+    assert_instance_of FileReader, file_reader
+  end
+end
