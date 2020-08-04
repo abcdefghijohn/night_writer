6f20349681abda9f312d3e95ee3c8da051e88702
John Kim <abcdefghijohnk@gmail.com>
2020-08-03 23:36:17 -0600

    more than 80 chars are split

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -5,9 +5,11 @@ file = FileReader.new
 translator = Translator.new
 
 file_contents = file.read(ARGV[0])
-
+split = file_contents.scan(/.{1,40}/)
+split.map do |string|
+translation = translator.to_braille(string)
 output = file.write(ARGV[1], translation)
-# end.join("\n")
+end.join("\n")
 # translation = translator.to_braille(file_contents)
 
 
