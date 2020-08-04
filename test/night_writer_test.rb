9f4e7d1a209ac07b41a313eec06ce94de9d371a2
John Kim <abcdefghijohnk@gmail.com>
2020-08-03 18:03:59 -0600

    create dictionary module for eng to braille

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -20,23 +20,7 @@ class NightWriterTest < MiniTest::Test
   end
 
   def test_it_can_display_initial_message
-    expected = expected = "Created 'braille.txt' containing 10 characters"
+    expected = expected = "Created 'braille.txt' containing 256 characters"
     assert_equal expected, @night_writer.initial_message
   end
 end
-
-  # def test_initial_message_prints
-  #   night_writer = NightWriter.new
-  #   night_writer.read_from_file
-  #   night_writer.write_to_file
-  #   expected = "Created 'braille.txt' containing 11 characters"
-  #   assert_equal expected, night_writer.initial_message
-  # end
-
-#   def test_it_can_display_initial_message
-#     ARGV.replace ["message.txt", "braille.txt"]
-#     night_writer = NightWriter.new
-#
-#     expected = "Created 'braille.txt' containing 11 characters"
-#     assert_equal expected, night_writer.initial_message
-#   end
