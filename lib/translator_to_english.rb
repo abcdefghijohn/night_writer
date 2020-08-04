0103de7395d8cb1b2d6ee2821d1ddb69c91bfa85
John Kim <abcdefghijohnk@gmail.com>
2020-08-04 00:52:07 -0600

    rewrote test for single element and method

--- lib/translator_to_english.rb
+++ lib/translator_to_english.rb
@@ -4,12 +4,18 @@ require './lib/file_reader'
 class TranslatorToEnglish
   include BrailleDictionary
 
-  def split_braille(message)
-    length = message.length / 3
-    message.scan(/.{1,#{length}}/)
-  end
-
-  def get_first_element(input)
-    input.fetch(0).scan(/.{1,2}/)
+#   def split_braille(message)
+#     length = message.length / 3
+#     message.scan(/.{1,#{length}}/)
+#   end
+#
+#   def get_first_element(input)
+#     input.fetch(0).scan(/.{1,2}/)
+#   end
+  def decode_one_element(element)
+    braille_array = input.split("\n")
+    braille_dictionary.select do |braille, letter|
+    return letter if braille_array == braille
+    end
   end
 end
--- test/translator_to_english_test.rb
+++ test/translator_to_english_test.rb
@@ -11,26 +11,26 @@ class TranslatorToEnglishTest < Minitest::Test
     assert_instance_of TranslatorToEnglish, translated
   end
 
-  def test_it_can_split_braille_characters
-    translated = TranslatorToEnglish.new
-    input = "0.0...00....\n00.00.0..0\n000...0000.."
-    expected = ["0.0...00....", "00.00.0..0", "000...0000.."]
-
-    assert_equal expected, translated.split_braille(input)
-  end
-
-  def test_it_can_return_first_element_of_braille_characters
-    translated = TranslatorToEnglish.new
-    input = ["0.0...00....", "00.00.0..0", "000...0000.."]
-    expected = ["0.", "0.", "..", "00", "..", ".."]
-
-    assert_equal expected, translated.get_first_element(input)
-  end
+  # def test_it_can_split_braille_characters
+  #   translated = TranslatorToEnglish.new
+  #   input = "0.0...00....\n00.00.0..0\n000...0000.."
+  #   expected = ["0.0...00....", "00.00.0..0", "000...0000.."]
+  #
+  #   assert_equal expected, translated.split_braille(input)
+  # end
+  #
+  # def test_it_can_return_first_element_of_braille_characters
+  #   translated = TranslatorToEnglish.new
+  #   input = ["0.0...00....", "00.00.0..0", "000...0000.."]
+  #   expected = ["0.", "0.", "..", "00", "..", ".."]
+  #
+  #   assert_equal expected, translated.get_first_element(input)
+  # end
 
   def test_it_can_decode_a_braille_element_to_a_letter
     skip
     translated = TranslatorToEnglish.new
-    actual = [["0.", "..", ".."]]
-    assert_equal ("a"), translated.to_english(actual)
+    actual = "0.\n..\n.."
+    assert_equal "a", translated.decode_one_element(actual)
   end
 end
