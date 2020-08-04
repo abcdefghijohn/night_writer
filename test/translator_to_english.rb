89db7a9f8e2d48e9d14cd1387a46ac04de543f61
John Kim <abcdefghijohnk@gmail.com>
2020-08-04 00:06:34 -0600

    create decode single braille character test

--- test/translator_to_english.rb
+++ test/translator_to_english.rb
@@ -10,4 +10,10 @@ class TranslatorToEnglishTest < Minitest::Test
     translator_to_english = TranslatorToEnglish.new
     assert_instance_of TranslatorToEnglish, translator_to_english
   end
+
+  def test_it_can_decode_a_braille_element_to_a_letter
+    translator = Translator.new
+    actual = [["0.", "..", ".."]]
+    assert_equal ("a"), translator_to_english.to_english(actual)
+  end
 end
